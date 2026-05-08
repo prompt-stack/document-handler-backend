87fffb6ba1a4ea2bb3ec1b039edc9ec8e8dd7af8
realynpatoy <realynpatoy@gmail.com>
2026-05-08 20:21:58 +0800

    Adding cors configuration

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,19 @@ import { config } from "dotenv";
 import { dbConnection } from "./config/dbConfig";
 import sequelize from './config/dbConfig';
 import { insertDefaultUser } from "./config/defaultUser";
+import cors from 'cors';
 
 config();
 
 const app = express();
 const PORT = process.env.PORT || 8081;
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+app.use(express.json());
 
 app.use(express.json());
 app.use(routes);
